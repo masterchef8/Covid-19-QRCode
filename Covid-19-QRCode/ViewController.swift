e8e1dac3489df8ff2d91c1b0c175345ebc44b42b
pgiraude <_XYZCargoBike__>
2020-04-06 11:55:01 +0200

    Initial Commit

---
+++ Covid-19-QRCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Covid-19-QRCode
+//
+//  Created by Somebody on 06/04/2020.
+//  Copyright © 2020 Somebody. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
